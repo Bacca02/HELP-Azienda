ac1a77e29cd96aef04aac89f79fee3ac7ff9c953
MAALFING <marco.soggiu@live.it>
2022-04-26 17:28:32 +0200

    Fix accesso che controllava male

--- php/accesso.php
+++ php/accesso.php
@@ -17,7 +17,7 @@ $name=$pass="";
             if($result = mysqli_query($link,$sql)){
                 if(mysqli_num_rows($result) > 0){
                     $row = mysqli_fetch_array($result);
-                    if($row['Password']==$pass&&$row['Tipo']=="admin"){                            
+                    if($row['Password']==$pass&&$row['Tipo']=="Admin"){                            
                         $age = array("Esito"=>"V","Tipo"=>"A");
 						echo json_encode($age);                        
                     exit();
